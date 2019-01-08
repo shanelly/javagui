4765635a8d1fe0e8a97f2557b4bdf8827b7a4149
shanelly <shanelle.encomienda@gmail.com>
2019-01-08 13:12:52 +0800

    added JFrame

--- BasicElements.java
+++ BasicElements.java
@@ -1,7 +1,17 @@
-public class BasicElemnts{
-	
-	public static void main (String[] args){
-		System.out.println("basic java elements");
+import javax.swing.JFrame;
 
+public class BasicElements{
+	
+	public static void main (String args[]){
+		JFrame myFrame= new JFrame();
+
+			myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
+			myFrame.setVisible(true);
+			myFrame.setSize(900,600);
+
+			System.out.println("basic java elements");
+
+
+		}
+			
 	}
-}
